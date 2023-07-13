e7dad36a53b22949048c816b21f13174e37c9f2d
Aray <wildtwitter.aray@gmail.com>
2023-07-14 03:25:06 +0530

    Added Interest Finder logic

--- Untitled-1.py
+++ Untitled-1.py
@@ -25,8 +25,8 @@ def add_attraction(destination, attraction):
     attractions_for_destination = attractions[destination_index]
     attractions_for_destination.append(attraction) 
     
-add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
-print(attractions)
+add_attraction("Los Angeles, USA", ["Venice Beach", ["beach"]])
+
 
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
 add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
@@ -38,6 +38,23 @@ add_attraction("Sao Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("Sao Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+
+def find_attractions(destination, interests):
+    destination_index = get_destination_index(destination)
+    attractions_in_city = attractions[destination_index]
+    attractions_with_interest = []
+    for possible_attraction in attractions_in_city: 
+        attraction_tags = possible_attraction[1] 
+        for interest in interests:
+            if interest in attraction_tags:
+                attractions_with_interest.append(possible_attraction[0])
+    return attractions_with_interest
+    
+la_arts = find_attractions("Los Angeles, USA", ["art"])
+print(la_arts)
+
+
 
 
 
